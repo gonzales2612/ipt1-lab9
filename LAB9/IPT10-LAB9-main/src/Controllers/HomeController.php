5ce1cbfdb8d22221d63529bfa5af6a9c6cff8c41
gonzales2612 <gonzalescarlo26@gmail.com>
2024-10-15 00:00:45 +0800

    Add files via upload

---
+++ LAB9/IPT10-LAB9-main/src/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Supplier;
+use App\Controllers\BaseController;
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+        $template = 'home';
+        $data = [
+            'student' => 'DON HENESSY S. DAVID',
+            'title' => 'IPT10 Laboratory Activity #9',
+            'college' => 'College of Computer Studies',
+            'university' => 'Angeles University Foundation',
+            'location' => 'Angeles City, Pampanga, Philippines'
+        ];
+        $output = $this->render($template, $data);
+        return $output;
+    }
+}
